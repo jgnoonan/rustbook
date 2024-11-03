ae014ef99153f753ae4db77f0c44316cbe7644b7
ripple-cny <jgnoonan@att.net>
2024-11-03 11:23:06 -0500

    Branches lesson file part 2

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,40 +1,13 @@
 fn main() {
-    let x = 1;
-    let y = if x != 1 { 0 } else { 1 }; 
-    println!("{y}");
-
-    loop {
-        println!("again!");
-        break;
+    let a = [5; 10];
+    let mut sum = 0;
+    for x in a {
+        sum += x;
     }
+    println!("{sum}");
 
-    let mut counter = 0;
-    let result = loop {
-        counter += 1;
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
-    println!("The result is {result}");
-
-    let mut count = 0;
-    'counting_up: loop {
-        println!("count = {count}");
-        let mut remaining = 10;
-
-        loop {
-            println!("remaining = {remaining}");
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
-
-        count += 1;
+    let a = Box::new([0; 500]);
+    for (i, elem) in a.iter().enumerate() {
+        println!("Index {:?}: {:?}", i, elem);
     }
-    println!("End count = {count}");
-    
   }
